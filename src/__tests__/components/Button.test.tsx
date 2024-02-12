54b64335bbf2fb7c7245217d98a11752edad3637
scsx <4182349+scsx@users.noreply.github.com>
2024-02-12 18:08:49 +0000

    Tests folder structure

--- src/__tests__/components/Button.test.tsx
+++ src/__tests__/components/Button.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import Button from '../components/Button'
+import Button from '../../components/Button'
 
 const handleClick = jest.fn()
 
