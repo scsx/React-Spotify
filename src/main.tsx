5236d05c33013b88725875ba08dff2be37a473c1
scsx <4182349+scsx@users.noreply.github.com>
2024-02-12 21:03:28 +0000

    Fonts and MUI applied

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { TokenProvider } from './contexts/TokenContext'
 import App from './App.tsx'
+import CssBaseline from '@mui/material/CssBaseline'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline />
     <TokenProvider>
       <BrowserRouter>
         <App />
