7c4e39e42abf5b75f7cd85995a4a54d7241b78d3
scsx <4182349+scsx@users.noreply.github.com>
2024-02-12 14:57:41 +0000

    test flags and ignore

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  rootDir: './',
+  coverageDirectory: '<rootDir>/coverage',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules'],
+  coverageReporters: ['json', 'html'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   }
