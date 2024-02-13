0beaf6ba8b7c335477199ce62dff4b6c86c8ed54
scsx <4182349+scsx@users.noreply.github.com>
2024-02-13 16:39:32 +0000

    Theme provider e spotify green

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import Button from './components/Button'
 import { Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from './contexts/ThemeProvider'
 
 import Homepage from './pages/Homepage'
 import Login from './pages/Login'
@@ -8,12 +9,12 @@ import './App.css'
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/login' element={<Login />} />
       </Routes>
-    </>
+    </ThemeProvider>
   )
 }
 
