28a3b852a1f2a7da11265bd7d5d30f6d16d528a9
scsx <4182349+scsx@users.noreply.github.com>
2024-02-13 01:06:11 +0000

    MUI config and colors

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,37 @@ import { BrowserRouter } from 'react-router-dom'
 import { TokenProvider } from './contexts/TokenContext'
 import App from './App.tsx'
 import CssBaseline from '@mui/material/CssBaseline'
-import './index.css'
+import { createTheme } from '@mui/material'
+import { ThemeProvider } from '@mui/material'
+
+const reactSpotifyTheme = createTheme({
+  typography: {
+    fontFamily: ['Gabarito', 'sans-serif'].join(',')
+  },
+  palette: {
+    primary: {
+      main: '#1DB954'
+    },
+    secondary: {
+      main: '#191414'
+    }
+    /*
+    background: {
+      default: '#191414'
+    }
+    */
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CssBaseline />
-    <TokenProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </TokenProvider>
+    <ThemeProvider theme={reactSpotifyTheme}>
+      <TokenProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </TokenProvider>
+    </ThemeProvider>
   </React.StrictMode>
 )
