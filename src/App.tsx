67ac12de3fd927a34963a9837e167846581bdbd4
scsx <4182349+scsx@users.noreply.github.com>
2024-02-14 13:50:24 +0000

    Header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import Button from './components/Button'
 import { Routes, Route } from 'react-router-dom'
+
 import { ThemeProvider } from './contexts/ThemeProvider'
 
 import Header from './components/Header'
