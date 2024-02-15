f2ce99976009cc459fdb528bd1d2fecb100d7d55
scsx <4182349+scsx@users.noreply.github.com>
2024-02-15 11:28:31 +0000

    welcome h1 width fixed

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -3,11 +3,15 @@ import { MdArrowOutward } from 'react-icons/md'
 const Welcome = (): JSX.Element => {
   return (
     <>
-      <h1 className='text-6xl lg:text-9xl bg-gradient-to-l from-primary to-black dark:to-white inline-block lg:w-1/2 text-transparent bg-clip-text'>
-        Welcome.
-      </h1>
+      <div className='block'>
+        <h1 className='inline-block text-6xl lg:text-9xl bg-gradient-to-l from-primary to-black dark:to-white text-transparent bg-clip-text'>
+          Welcome.
+        </h1>
+      </div>
       <hr />
-      <p className='text-2xl pt-4'  style={{ boxShadow: 'rgb(0 0 0 / 11%) 0px -13px 14px -10px' }}>
+      <p
+        className='text-2xl pt-4'
+        style={{ boxShadow: 'rgb(0 0 0 / 11%) 0px -13px 14px -10px' }}>
         Please authenticate, over there{' '}
         <MdArrowOutward className='inline text-4xl animate-pulse' />
       </p>
