a2e497ccd796e33f559d60e04e30d98546ceff0d
scsx <4182349+scsx@users.noreply.github.com>
2024-02-15 12:12:24 +0000

    Artist response html

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import axios from 'axios'
-import { Link } from 'react-router-dom'
 import { useToken } from '../contexts/TokenContext'
 import { SpotifyArtist } from '../types/SpotifyArtist'
 import Welcome from '@/components/Welcome'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
 
-interface SpotifySearchResponse {
+interface SpotifySearchArtistsResponse {
   artists: {
     items: SpotifyArtist[]
   }
@@ -20,7 +27,7 @@ const Homepage = (): JSX.Element => {
   // Search Artists.
   const searchArtists = async (e: React.FormEvent): Promise<void> => {
     e.preventDefault()
-    const { data }: { data: SpotifySearchResponse } = await axios.get(
+    const { data }: { data: SpotifySearchArtistsResponse } = await axios.get(
       'https://api.spotify.com/v1/search',
       {
         headers: {
@@ -40,31 +47,55 @@ const Homepage = (): JSX.Element => {
   // Render Artists.
   const renderArtists = (): JSX.Element[] => {
     if (artists.length > 0) {
-      return artists.map((artist) => {
+      return artists.map((artist, index) => {
+
+        // MAKE Least Popular results blurry and blurryer
+
         return (
-          <div key={artist.id}>
-            <p>Image here</p>
-            {artist.name}
-          </div>
+          <Card
+            key={artist.id}
+            className={`col-span-3 lg:col-span-1 mb-1 ${
+              index === 0 ? 'lg:col-span-full' : ''
+            }`}>
+            <CardHeader>
+              <CardTitle>{artist.name}</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Popularity: {artist.popularity}</p>
+            </CardFooter>
+          </Card>
         )
       })
     } else {
-      return [<h2>No artists to display.</h2>]
+      return [<h2 key={'noresults'}>No artists to display.</h2>] // In array so it always returns JSX.Element[]. Forces key.
     }
   }
 
   return (
     <div className='home container flex flex-col flex-1 justify-center'>
       {token ? (
-        <form onSubmit={searchArtists}>
-          <input type='text' onChange={(e) => setSearchKey(e.target.value)} />
-          <button type='submit'>Search</button>
-        </form>
+        <>
+          <h1>Search Artists</h1>
+          <form onSubmit={searchArtists}>
+            <input type='text' onChange={(e) => setSearchKey(e.target.value)} />
+            <button type='submit'>Search</button>
+          </form>
+        </>
       ) : (
         <Welcome />
       )}
 
-      <div>{renderArtists()}</div>
+      {artists.length > 0 && (
+        <>
+          <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+            {renderArtists()}
+          </div>
+        </>
+      )}
     </div>
   )
 }
