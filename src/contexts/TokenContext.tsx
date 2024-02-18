fe86ced9cae86da1274f8a1f62264af60beed9c5
scsx <4182349+scsx@users.noreply.github.com>
2024-02-18 04:34:28 +0000

    Header cleanup

--- src/contexts/TokenContext.tsx
+++ src/contexts/TokenContext.tsx
@@ -6,15 +6,27 @@ import {
   ReactNode
 } from 'react'
 
-const TokenContext = createContext('')
+interface TokenInfo {
+  isValid: boolean
+  authLink: string
+}
+
+const TokenContext = createContext<TokenInfo | null>(null)
 export const useToken = () => useContext(TokenContext)
+
+const VITE_SPOTIFY_CLIENT_ID = process.env.VITE_SPOTIFY_CLIENT_ID
+const VITE_SPOTIFY_REDIRECT_URI = process.env.VITE_SPOTIFY_REDIRECT_URI
+const VITE_SPOTIFY_AUTH_ENDPOINT = process.env.VITE_SPOTIFY_AUTH_ENDPOINT
+const VITE_SPOTIFY_RESPONSE_TYPE = process.env.VITE_SPOTIFY_RESPONSE_TYPE
+
+const authLink = `${VITE_SPOTIFY_AUTH_ENDPOINT}?client_id=${VITE_SPOTIFY_CLIENT_ID}&redirect_uri=${VITE_SPOTIFY_REDIRECT_URI}&response_type=${VITE_SPOTIFY_RESPONSE_TYPE}`
 
 interface TokenProviderProps {
   children: ReactNode
 }
 
 export const TokenProvider: React.FC<TokenProviderProps> = ({ children }) => {
-  const [token, setToken] = useState('')
+  const [token, setToken] = useState<TokenInfo | null>(null)
 
   // Set token.
   useEffect(() => {
@@ -32,8 +44,17 @@ export const TokenProvider: React.FC<TokenProviderProps> = ({ children }) => {
       window.localStorage.setItem('spotifyToken', spotifyToken ?? '')
     }
 
-    setToken(spotifyToken ?? '')
+    setToken({
+      isValid: true,
+      authLink: authLink
+    })
   }, [])
 
-  return <TokenContext.Provider value={token}>{children}</TokenContext.Provider>
+  const tokenFallback = token ?? { isValid: false, authLink: '' }
+
+  return (
+    <TokenContext.Provider value={tokenFallback}>
+      {children}
+    </TokenContext.Provider>
+  )
 }
