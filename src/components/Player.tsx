437d579c95b83645d7b5da802722339a017e6e93
scsx <4182349+scsx@users.noreply.github.com>
2024-02-20 23:32:53 +0000

    UI

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -2,14 +2,13 @@ import { useState, useEffect } from 'react'
 import { SpotifyArtist } from '@/types/SpotifyArtist'
 import { getCurrentlyPlaying } from '@/services/SpotifyPlayer'
 import { useToken } from '../contexts/TokenContext'
+import { SpotifyAlbum } from '@/types/SpotifyAlbum'
 
+// Interfaces for Player are simpler and not complete.
 interface NowPlaying {
-  data: {
-    item: {
-      name: string
-      artists: SpotifyArtist[]
-    }
-  }
+  album: SpotifyAlbum
+  name: string
+  artists: SpotifyArtist[]
 }
 
 const Player = (): JSX.Element => {
@@ -22,7 +21,7 @@ const Player = (): JSX.Element => {
     const fetchData = async () => {
       try {
         const playing = await getCurrentlyPlaying()
-        setNowPlaying(playing)
+        setNowPlaying(playing.data.item)
         console.log(playing)
       } catch (error) {
         console.error('Error fetching currently playing:', error)
@@ -36,11 +35,14 @@ const Player = (): JSX.Element => {
 
   return (
     <div>
-      {nowPlaying && nowPlaying?.data && (
-        <>
-          <p>{nowPlaying && nowPlaying?.data?.item?.name}</p>
-          <p>{nowPlaying && nowPlaying?.data?.item?.artists[0]?.name}</p>
-        </>
+      {nowPlaying && (
+        <div className='flex'>
+          <img src={nowPlaying.album?.images[2].url} alt={nowPlaying.name} width={48} height={48} className='mr-2' />
+          <div>
+            <p className='text-gray-700 dark:text-gray-300'>{nowPlaying.name}</p>
+            <p className='text-gray-500'>{nowPlaying.artists[0]?.name}</p>
+          </div>
+        </div>
       )}
     </div>
   )
--- src/types/SpotifyAlbum.ts
+++ src/types/SpotifyAlbum.ts
@@ -1,23 +1,45 @@
 import { SpotifyImage } from "./SpotifyImage"
 import { SpotifyArtist } from "./SpotifyArtist"
+import { SpotifyTrack } from "./SpotifyTrack";
 
 export interface SpotifyAlbum {
-  album_type: string
-  total_tracks: number
-  available_markets: string[]
+  album_type: string;
+  total_tracks: number;
+  available_markets: string[];
   external_urls: {
-    spotify: string
-  }
-  href: string
-  id: string
-  images: SpotifyImage[]
-  name: string
-  release_date: string
-  release_date_precision: string
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  images: SpotifyImage[];
+  name: string;
+  release_date: string;
+  release_date_precision: string;
   restrictions: {
-    reason: string
-  }
-  type: string
-  uri: string
-  artists: SpotifyArtist[]
+    reason: string;
+  };
+  type: string;
+  uri: string;
+  artists: SpotifyArtist[];
+  tracks: {
+    href: string;
+    limit: number;
+    next: string;
+    offset: number;
+    previous: string;
+    total: number;
+    items: SpotifyTrack[];
+  };
+  copyrights: {
+    text: string;
+    type: string;
+  }[];
+  external_ids: {
+    isrc: string;
+    ean: string;
+    upc: string;
+  };
+  genres: string[];
+  label: string;
+  popularity: number;
 }
