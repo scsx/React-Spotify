74cf4dd09eb59e1d42cf737d6eaa02dd27109821
scsx <4182349+scsx@users.noreply.github.com>
2024-02-23 19:29:53 +0000

    Artist UI

--- src/pages/Artist.tsx
+++ src/pages/Artist.tsx
@@ -41,11 +41,16 @@ const Artist = (): JSX.Element => {
             }}></div>
           <div className='w-full -mt-4 absolute top-96 h-8 z-0 bg-white dark:bg-background transition duration-500'></div>
           <div className='relative container'>
-            <div className='bg-white dark:bg-black inline-block p-4 rounded-sm mb-16'>
+            <div className='bg-white dark:bg-black inline-block p-4 rounded-sm rounded-bl-none'>
               <HeadingOne
                 text={artist.name}
                 classes='text-3xl md:text-6xl font-semibold mb-0 tracking-wide'
               />
+            </div>
+            <div className='mb-6 '>
+              <p className='bg-white dark:bg-black inline-block w-auto py-2 px-4 rounded-bl-sm rounded-br-sm'>
+                {artist.followers.total.toLocaleString()} followers
+              </p>
             </div>
 
             <div className='grid grid-cols-4 gap-8'>
@@ -53,7 +58,7 @@ const Artist = (): JSX.Element => {
                 <Albums />
               </div>
               <div className='col-span-2 pt-16'>
-                <div className='grid grid-cols-3 gap-8 -mt-20'>
+                <div className='grid grid-cols-3 gap-8 -mt-20 mb-8'>
                   <div className='col-start-2 col-end-4'>
                     <AspectRatio ratio={1 / 1}>
                       <img
@@ -64,7 +69,7 @@ const Artist = (): JSX.Element => {
                     </AspectRatio>
                   </div>
                 </div>
-                <p>{artist.followers.total.toLocaleString()} followers</p>
+                <TopTracks artistId={artist.id} />
               </div>
             </div>
           </div>
