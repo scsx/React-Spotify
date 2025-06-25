4c37e21946817f913dc05166bc00cab5b8743056
scsx <4182349+scsx@users.noreply.github.com>
2025-06-25 14:42:58 +0100

    Searcg results type updated

--- frontend/src/types/SpotifySearchResults.ts
+++ frontend/src/types/SpotifySearchResults.ts
@@ -1,9 +1,24 @@
+import { TSpotifyAlbum } from './SpotifyAlbum'
 import { TSpotifyArtist } from './SpotifyArtist'
 import { TSpotifyTrack } from './SpotifyTrack'
 
-export interface TSpotifySearchResults {
-  items: TSpotifyArtist[] | TSpotifyTrack[]
-  nextPage: string
-  prevPage: string
+export interface TSpotifyPagingObject<T> {
+  href: string
+  items: T[]
+  limit: number
+  next: string | null
+  offset: number
+  previous: string | null
   total: number
+}
+
+export interface TSpotifySearchResults {
+  tracks?: TSpotifyPagingObject<TSpotifyTrack>
+  artists?: TSpotifyPagingObject<TSpotifyArtist>
+  albums?: TSpotifyPagingObject<TSpotifyAlbum>
+  // Non-existing so far.
+  // playlists?: TSpotifyPagingObject<TSpotifyPlaylist>
+  // shows?: TSpotifyPagingObject<TSpotifyShow>
+  // episodes?: TSpotifyPagingObject<TSpotifyEpisode>
+  //audiobooks?: TSpotifyPagingObject<TSpotifyAudiobook>
 }
