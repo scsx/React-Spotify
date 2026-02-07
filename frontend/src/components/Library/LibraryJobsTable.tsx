8af1dc23447b6012e6dcb687544e7fad0bdb58b2
Jorge Soucasaux Monteiro <jorge.monteiro@vodafone.com>
2026-02-07 21:09:09 +0000

    download job UI

--- frontend/src/components/Library/LibraryJobsTable.tsx
+++ frontend/src/components/Library/LibraryJobsTable.tsx
@@ -2,10 +2,10 @@ import { TLibraryJobsTableProps } from '@/types/Library'
 import * as Tooltip from '@radix-ui/react-tooltip'
 import { AiOutlineDelete } from 'react-icons/ai'
 import { FaCheckCircle } from 'react-icons/fa'
-import { FaRegFloppyDisk } from 'react-icons/fa6'
+import { IoMdDownload } from 'react-icons/io'
 import { IoWarningOutline } from 'react-icons/io5'
 import { IoCloseSharp } from 'react-icons/io5'
-import { LuEye } from 'react-icons/lu'
+import { LuDatabaseBackup, LuEye } from 'react-icons/lu'
 
 import Text from '@/components/shared/Text'
 import {
@@ -98,7 +98,7 @@ export default function LibraryJobsTable({
             .map((job) => (
               <TableRow key={job.id}>
                 <TableCell className="font-mono text-sm">
-                  {job.id?.slice(0, 14) || 'n/a'}...
+                  {job.id?.slice(0, 20) || 'n/a'}...
                 </TableCell>
                 <TableCell className="flex items-center">
                   <span
@@ -137,32 +137,36 @@ export default function LibraryJobsTable({
                   })()}
                 </TableCell>
 
-                <TableCell className="flex items-center gap-4">
+                <TableCell className="flex items-center gap-2">
                   <Tooltip.Provider>
                     <Tooltip.Root>
                       <Tooltip.Trigger asChild>
                         <button
-                          className="text-2xl hover:text-blue-500"
+                          className="text-2xl hover:text-blue-500 px-2 py-1"
                           onClick={() => onPreview(job.id)}
                           disabled={loading}
                         >
                           <LuEye />
                         </button>
                       </Tooltip.Trigger>
-                      <Tooltip.Content>Preview job details</Tooltip.Content>
+                      <Tooltip.Content className="bg-blue-500 text-white px-3 py-1 rounded text-sm mb-4">
+                        Preview job details
+                      </Tooltip.Content>
                     </Tooltip.Root>
 
                     <Tooltip.Root>
                       <Tooltip.Trigger asChild>
                         <button
-                          className="text-2xl hover:text-red-500"
+                          className="text-2xl hover:text-red-500 px-2 py-1"
                           onClick={() => onDelete(job.id)}
                           disabled={loading}
                         >
                           <AiOutlineDelete />
                         </button>
                       </Tooltip.Trigger>
-                      <Tooltip.Content>Delete job</Tooltip.Content>
+                      <Tooltip.Content className="bg-red-700 text-white px-3 py-1 rounded text-sm mb-4">
+                        Delete job
+                      </Tooltip.Content>
                     </Tooltip.Root>
                   </Tooltip.Provider>
                 </TableCell>
@@ -174,20 +178,48 @@ export default function LibraryJobsTable({
                       if (!formatted) return 'n/a'
                       const [date, daysAgo] = formatted.split(' ')
                       return (
-                        <div className="flex gap-x-2 justify-end">
-                          <Text className="text-sm text-primary">{date}</Text>
-                          <Text className="text-sm text-primary">{daysAgo}</Text>
-                        </div>
+                        <>
+                          <div className="flex gap-x-2 justify-end items-center">
+                            <Text className="text-sm">Saved at {date}</Text>
+                            <Text className="text-sm">{daysAgo}</Text>
+                          </div>
+                          <Tooltip.Provider>
+                            <Tooltip.Root>
+                              <Tooltip.Trigger asChild>
+                                <div className="flex gap-x-2 justify-end items-center mt-4 cursor-pointer hover:text-primary">
+                                  <Text className="text-sm hover:text-primary">
+                                    Download as JSON
+                                  </Text>
+                                  <button>
+                                    <IoMdDownload className="text-2xl" />
+                                  </button>
+                                </div>
+                              </Tooltip.Trigger>
+                              <Tooltip.Content className="bg-green-600 px-3 py-1 rounded text-sm mb-4">
+                                Download job data
+                              </Tooltip.Content>
+                            </Tooltip.Root>
+                          </Tooltip.Provider>
+                        </>
                       )
                     })()
                   ) : (
-                    <button
-                      onClick={() => onSaveToIndexDB(job.id)}
-                      className="text-xl hover:text-primary"
-                      disabled={loading}
-                    >
-                      <FaRegFloppyDisk />
-                    </button>
+                    <Tooltip.Provider>
+                      <Tooltip.Root>
+                        <Tooltip.Trigger asChild>
+                          <button
+                            onClick={() => onSaveToIndexDB(job.id)}
+                            className="text-xl hover:text-primary"
+                            disabled={loading}
+                          >
+                            <LuDatabaseBackup />
+                          </button>
+                        </Tooltip.Trigger>
+                        <Tooltip.Content className="bg-blue-500 px-3 py-1 rounded text-sm mb-4">
+                          Save to IndexDB (browser)
+                        </Tooltip.Content>
+                      </Tooltip.Root>
+                    </Tooltip.Provider>
                   )}
                 </TableCell>
               </TableRow>
